5af6b22650d59fe5841d4fb4dd73cc4a82d92863
Oleg <o.sas@aisnovations.com>
2017-04-05 15:59:32 +0300

    use MongoDB

--- client/main.js
+++ client/main.js
@@ -1,19 +1,18 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
- 
+import { Parties } from '../collections/parties';
+
 angular.module('socially', [
   angularMeteor
 ])
 .controller('PartiesListCtrl', function($scope) {
     'ngInject';
-    $scope.parties = [{
-      'name': 'Dubstep-Free Zone',
-      'description': 'Can we please just for an evening not listen to dubstep.'
-    }, {
-      'name': 'All dubstep all the time',
-      'description': 'Get it on!'
-    }, {
-      'name': 'Savage lounging',
-      'description': 'Leisure suit required. And only fiercest manners.'
-    }];
+    $scope.helpers({
+      parties() {
+        return Parties.find({});
+      
+      }
+    });
+    // console.log($scope.parties);
+    // $scope.parties.forEach((party) => {console.log(party)});
   });
