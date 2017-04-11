42ad72d3e0a95cbf803a950fd26685f9c516fee6
Oleg <o.sas@aisnovations.com>
2017-04-11 10:48:49 +0300

    create navigation

--- imports/ui/components/socially/socially.js
+++ imports/ui/components/socially/socially.js
@@ -4,6 +4,7 @@ import uiRouter from 'angular-ui-router';
 
 import template from './socially.html';
 import { name as PartiesList } from '../partiesList/partiesList';
+import { name as Navigation } from '../navigation/navigation';
 
 class Socially {}
 
@@ -13,9 +14,18 @@ const name = 'socially';
 export default angular.module(name, [
     angularMeteor,
     uiRouter,
-    PartiesList
+    PartiesList,
+    Navigation
 ]).component(name, {
     template,
     controllerAs: name,
     controller: Socially
-});
+}).config(config);
+
+function config($locationProvider, $urlRouterProvider) {
+    'ngInject';
+
+    $locationProvider.html5Mode(true);
+
+    $urlRouterProvider.otherwise('/parties');
+}
