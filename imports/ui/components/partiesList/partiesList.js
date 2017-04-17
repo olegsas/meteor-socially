959f9b6cf8c57982bc3161007f0544415f1c0fbb
Oleg <o.sas@aisnovations.com>
2017-04-17 17:49:23 +0300

    add subscribe

--- imports/ui/components/partiesList/partiesList.js
+++ imports/ui/components/partiesList/partiesList.js
@@ -14,6 +14,8 @@ class PartiesList {
 
         $reactive(this).attach($scope);
 
+        this.subscribe('parties');
+
         this.helpers({
             parties() {
                 return Parties.find({});
