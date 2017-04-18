35b4ca2d28072a54c859afea8e51c54d14b3061e
Oleg <o.sas@aisnovations.com>
2017-04-18 14:18:27 +0300

    Subscribe works OK. Let's invite friends!

--- imports/ui/components/partyDetails/partyDetails.js
+++ imports/ui/components/partyDetails/partyDetails.js
@@ -35,7 +35,8 @@ class PartyDetails {
         }, {
             $set: {
                 name: this.party.name,
-                description: this.party.description
+                description: this.party.description,
+                public: this.party.public
             }
         }
         , (error) => {
